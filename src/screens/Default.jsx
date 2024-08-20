f38c547ac5122b3850151ac158af2651abf466e6
AFF40 <alejandro.fernando.flores99@gmail.com>
2024-08-20 11:43:11 -0400

    adding redux

--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,19 +1,22 @@
 import Logo from './_components/Logo'; 
 import Description from './_components/Description';
-
-
 import OpenLink from '../components/OpenLink';
+import LessonInfo from './_components/LessonInfo';
 
 const Default = () => {
 return (
+    <>
     <header className="App-header">
         <Logo />
         <Description />
-
         <OpenLink title="Learn React" url="https://reactjs.dev/" />
         <OpenLink title="mi proyecto" url="https://github.com/AFF40/desarrollo-frontend-react" />
-        
+        <LessonInfo unitNumber="1" title="introduccion a react y estructura de proyecto"/>
+        <LessonInfo unitNumber="2" title="context api para la gestion del estado global en aplicaciones react"/>
+
       </header>
+    </>
     );
-}
+};
+
 export default Default;
